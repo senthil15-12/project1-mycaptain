087e94ffb9d2cd6ee8d340d6570874cf4ace749a
senthil15-12 <67093765+senthil15-12@users.noreply.github.com>
2020-06-18 13:57:27 +0530

    Add files via upload
    program's to find area of a circle and extension of a file

---
+++ pro1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#program 1
+r=float(input("input the radius of the circle :"))
+a= 3.14*r*r
+print("The area of the circle with radius r is:",r)
+print(a)
+
+
+# In[ ]:
+
+
+#program 2
+filename=input("Enter a filename with extension")
+import numpy as n
+array1=n.array( filename )
+while (array1!="."):
+    array1-=1
+    if(array1!="."):
+        print(array1)
+    else:
+            break
+print(array1)
+
+
+# In[10]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
